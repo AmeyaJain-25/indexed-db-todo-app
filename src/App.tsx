0e6fb0d90c445632031c213f94c87eb274ad9143
AmeyaJain-25 <ameiya.jain@gmail.com>
2024-05-14 18:17:18 +0530

    react-virtualized added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { List } from 'react-virtualized';
+
+const data = new Array(1000000).fill('HI');
+
 function App() {
-  return <div className="App">HI</div>;
+  return (
+    <div className="App">
+      <List
+        height={400}
+        rowCount={data.length}
+        rowHeight={60}
+        rowRenderer={({ index, key, style }) => (
+          <div key={key} style={style}>
+            {data[index]}
+          </div>
+        )}
+        width={300}
+      />
+    </div>
+  );
 }
 
 export default App;
